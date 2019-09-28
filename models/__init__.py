f7cc715c721c55151ecc32d5ebe0c7aedd2a0c07
JoseRodriguesVieira <jose.rodrigues@rsinet.com.br>
2019-09-28 16:13:45 -0300

    aplication

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# Connect to database with sqlalchemy.
-db = SQLAlchemy()
-
-# Define models
-from .usuario import Usuario
--- routes/transferencia.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-transferencia = Blueprint('transferencia', __name__, template_folder='templates', static_folder='static') 
-
-@transferencia.route('/transferir', methods=['POST'])
-def index():
-	return 'GET tranferir'
